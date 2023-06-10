bb967beb7e33e8b4b727a70b03273b368401c935
cruway <astralkaiser89@gmail.com>
2023-06-10 23:08:31 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,22 @@ import 'package:crutoon/widgets/currency_card.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const App());
+  //runApp(const App());
+  runApp(const App2());
+}
+
+class App2 extends StatelessWidget {
+  const App2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Container(),
+      ),
+    );
+  }
 }
 
 class App extends StatelessWidget {
