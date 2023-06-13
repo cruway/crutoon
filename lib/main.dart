59ccce34b406b13d423397bf87bbeb7ad7328e9c
cruway <astralkaiser89@gmail.com>
2023-06-13 17:03:43 +0900

    pomodoro app setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,220 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  //runApp(const App());
-  runApp(const App2());
+  runApp(const App());
 }
 
-class App2 extends StatefulWidget {
-  const App2({super.key});
+class App extends StatefulWidget {
+  const App({super.key});
 
   @override
-  State<App2> createState() => _App2State();
+  State<App> createState() => _AppState();
 }
 
-class _App2State extends State<App2> {
-  // int counter = 0;
-
-  // void onClicked() {
-  //   setState(() {
-  //     counter = counter + 1;
-  //   });
-  // }
-
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
         textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red,
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
           ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
       home: const Scaffold(
-        backgroundColor: Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              MyLargeTitle(),
-              // Text(
-              //   '$counter',
-              //   style: const TextStyle(
-              //     fontSize: 30,
-              //   ),
-              // ),
-              // IconButton(
-              //   iconSize: 40,
-              //   onPressed: onClicked,
-              //   icon: const Icon(
-              //     Icons.add_box_rounded,
-              //   ),
-              // ),
-            ],
-          ),
-        ),
+        body: Center(),
       ),
     );
   }
 }
-
-class MyLargeTitle extends StatelessWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'MyLargeTitle',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge!.color,
-      ),
-    );
-  }
-}
-
-// class App extends StatelessWidget {
-//   const App({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         backgroundColor: const Color(0xFF181818),
-//         body: SingleChildScrollView(
-//           child: Padding(
-//             padding: const EdgeInsets.symmetric(
-//               horizontal: 20,
-//             ),
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 const SizedBox(
-//                   height: 80,
-//                 ),
-//                 Row(
-//                   mainAxisAlignment: MainAxisAlignment.end,
-//                   children: [
-//                     Column(
-//                       crossAxisAlignment: CrossAxisAlignment.end,
-//                       children: [
-//                         const Text(
-//                           'Hey, Selena',
-//                           style: TextStyle(
-//                             color: Colors.white,
-//                             fontSize: 28,
-//                             fontWeight: FontWeight.w800,
-//                           ),
-//                         ),
-//                         Text(
-//                           'Welcome back',
-//                           style: TextStyle(
-//                             color: Colors.white.withOpacity(0.8),
-//                             fontSize: 18,
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                   ],
-//                 ),
-//                 const SizedBox(
-//                   height: 70,
-//                 ),
-//                 Text(
-//                   'Total Balance',
-//                   style: TextStyle(
-//                     fontSize: 22,
-//                     color: Colors.white.withOpacity(0.8),
-//                   ),
-//                 ),
-//                 const SizedBox(
-//                   height: 10,
-//                 ),
-//                 const Text(
-//                   '\$5 194 482',
-//                   style: TextStyle(
-//                     fontSize: 48,
-//                     fontWeight: FontWeight.w600,
-//                     color: Colors.white,
-//                   ),
-//                 ),
-//                 const SizedBox(
-//                   height: 30,
-//                 ),
-//                 const Row(
-//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                   children: [
-//                     Button(
-//                       text: 'Transfer',
-//                       bgColor: Color(0xFFF1B33B),
-//                       textColor: Colors.black,
-//                     ),
-//                     Button(
-//                       text: 'Request',
-//                       bgColor: Color(0xFF1F2123),
-//                       textColor: Colors.white,
-//                     ),
-//                   ],
-//                 ),
-//                 const SizedBox(
-//                   height: 100,
-//                 ),
-//                 Row(
-//                   crossAxisAlignment: CrossAxisAlignment.end,
-//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                   children: [
-//                     const Text(
-//                       'Wallets',
-//                       style: TextStyle(
-//                         color: Colors.white,
-//                         fontSize: 36,
-//                         fontWeight: FontWeight.w600,
-//                       ),
-//                     ),
-//                     Text(
-//                       'View All',
-//                       style: TextStyle(
-//                         color: Colors.white.withOpacity(0.8),
-//                         fontSize: 18,
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//                 const SizedBox(
-//                   height: 20,
-//                 ),
-//                 const CurrencyCard(
-//                   name: 'Euro',
-//                   code: 'EUR',
-//                   amount: '6 428',
-//                   icon: Icons.euro_rounded,
-//                   isInverted: false,
-//                   order: 1,
-//                 ),
-//                 const CurrencyCard(
-//                   name: 'Bitcoin',
-//                   code: 'BTC',
-//                   amount: '9 785',
-//                   icon: Icons.currency_bitcoin,
-//                   isInverted: true,
-//                   order: 2,
-//                 ),
-//                 const CurrencyCard(
-//                   name: 'Dollar',
-//                   code: 'USD',
-//                   amount: '428',
-//                   icon: Icons.attach_money_outlined,
-//                   isInverted: false,
-//                   order: 3,
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
